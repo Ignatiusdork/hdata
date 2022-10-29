75f97f3194a35435e88eb3caf0034a016363818c
ignatius <Littignatius@gmail.com>
2022-10-29 16:11:24 +0100

    d1st commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    slackUsername: 'littignatius',
+    backend: true,
+    age: 27,
+    biography: 'I am a backend dev interning at HNG, looking forward to make the best out of this internship.',
+
+  });
+});
+
+module.exports = app;
